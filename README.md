475a064b75ff676e2ff8a8e1793355ec09d81297
weiyinfu <wei.yinfu@qq.com>
2020-06-06 05:26:13 +0800

    over

---
+++ README.md
@@ -0,0 +1,7 @@
+网上流传的四大名著txt版本有诸多错误，乱码、错字之类的错误不胜枚举，让人眼中读着心中堵着。本Repo根据一个纸质版书籍的pdf为基础，生成四大名著的txt版，错别字、乱码问题不可能有（如果有那就是原来的书籍就有）。
+
+
+```shell script
+pip install pdfminer3k
+python3 parse.py
+```
---
+++ parse.py
@@ -0,0 +1,50 @@
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LTTextBoxHorizontal, LAParams, LTFigure, LTCurve
+from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
+import logging
+from tqdm import tqdm
+
+# 不显示warning
+logging.propagate = False
+logging.getLogger().setLevel(logging.ERROR)
+
+pdf_filename = "./四大名著.pdf"
+txt_filename = "out.txt"
+
+device = PDFPageAggregator(PDFResourceManager(), laparams=LAParams())
+interpreter = PDFPageInterpreter(PDFResourceManager(), device)
+
+doc = PDFDocument()
+parser = PDFParser(open(pdf_filename, 'rb'))
+parser.set_document(doc)
+doc.set_parser(parser)
+doc.initialize()
+
+# 检测文档是否提供txt转换，不提供就忽略
+if not doc.is_extractable:
+    raise PDFTextExtractionNotAllowed
+else:
+    with open(txt_filename, 'w', encoding="utf-8") as fw:
+        print("num page:{}".format(len(list(doc.get_pages()))))
+        for page in tqdm(list(doc.get_pages()), desc='parsing'):
+            interpreter.process_page(page)
+            # 接受该页面的LTPage对象
+            layout = device.get_result()
+            # 这里layout是一个LTPage对象 里面存放着 这个page解析出的各种对象
+            # 一般包括LTTextBox, LTFigure, LTImage, LTTextBoxHorizontal 等等
+            # 想要获取文本就获得对象的text属性，
+            for x in layout:
+                if isinstance(x, LTTextBoxHorizontal):
+                    results = x.get_text()
+                    results = ''.join(i.strip() for i in results.splitlines())
+                    fw.write(results)
+                    fw.write('\n')
+                elif isinstance(x, LTFigure):
+                    pass
+                elif isinstance(x, LTCurve):
+                    pass
+                else:
+                    print(x)
+                    assert False
